0b2a99c1b739ee25ba9bc8c44db2b0a59d1dd99a
George Osafo-Osei Jonathan <georonathan47@gmail.com>
2023-02-02 12:20:37 +0000

    plaster build

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       theme: ThemeData(
           // primarySwatch: Color(0xFFFFFFFFF),
           ),
